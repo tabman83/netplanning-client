4ef36976867e3bad69217ab04b919c61e28d2583
tabman83 <tabman83@gmail.com>
2015-04-23 15:31:49 +0100

    Calendar component, WiP.

---
+++ www/js/directives/ion-calendar.js
@@ -0,0 +1,107 @@
+angular.module('NetPlanningApp').directive('ionCalendar', function($compile) {
+
+    function appendHeader(html) {
+        var header = angular.element('<table></table>').attr('width', '100%').addClass('header').addClass('stable-bg');
+        html.append(header);
+        var tr = angular.element('<tr></tr>');
+        header.append(tr);
+        for( var i=0; i<7; i++ ) {
+            var label = moment().day(i).format('dd');
+            var td = angular.element('<td></td>').html(label);
+            if(i === 0 || i === 6 ) {
+                td.addClass('assertive');
+            }
+            tr.append(td);
+        }
+    }
+
+
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            items: '='
+        },
+        compile: function($html, $attrs) {
+
+            $html.empty();
+            var root = angular.element('<div></div>').addClass('calendar');
+            appendHeader(root);
+            var content = angular.element('<ion-content></ion-content>');
+            root.append(content);
+            $html.append(root);
+
+            var monthObj = [];
+
+            var today = moment();
+            var startOfMonth = today.clone().startOf('month');
+            var endOfMonth = today.clone().endOf('month');
+
+            for( var day = startOfMonth.clone().subtract(startOfMonth.day(), 'day'); day.isBefore(startOfMonth, 'day'); day.add(1,'day') ) {
+                monthObj.push({
+                    type: 'past',
+                    m: day.clone()
+                });
+            }
+            for( var day = startOfMonth.clone(); day.isBefore(endOfMonth, 'day') || day.isSame(endOfMonth, 'day'); day.add(1,'day') ) {
+                monthObj.push({
+                    type: 'current',
+                    m: day.clone()
+                });
+            }
+            for( var day = endOfMonth.clone().add(1,'day'); day.isBefore( endOfMonth.clone().subtract(1, 'day').add(endOfMonth.day(), 'day') ); day.add(1,'day') ) {
+                monthObj.push({
+                    type: 'next',
+                    m: day.clone()
+                });
+            }
+
+            var table = angular.element('<table></table>').attr('width', '100%').addClass('month')
+            content.append(table);
+            tr = angular.element('<tr></tr>');
+            table.append(tr);
+            for( var i=0; i<7; i++ ) {
+                var monthEl = monthObj[i];
+                var className = monthEl.type+'-month';
+                var td = angular.element('<td></td>').addClass(className);
+                if(today.startOf('month').day() === i) {
+                    td.addClass('assertive').html(today.format('MMM').toUpperCase());
+                }
+                tr.append(td);
+            }
+
+
+            var table = angular.element('<table></table>').attr('width', '100%').addClass('content')
+            content.append(table);
+            for( var i=0; i<monthObj.length; i++) {
+                var monthEl = monthObj[i];
+                var tr;
+                if( i % 7 === 0 ) {
+                    tr = angular.element('<tr></tr>');
+                    table.append(tr);
+                }
+                var className = monthEl.type+'-month';
+                var td = angular.element('<td></td>').addClass(className);
+                //.addClass('badge badge-assertive')
+                var htmlDay = angular.element('<span></span>')
+                    .html(monthEl.m.format('D'))
+                    .addClass(monthEl.m.isSame(moment(), 'day') ? 'badge badge-assertive': '')
+                    .wrap('<div></div>')
+                    .parent();
+                td.append(htmlDay);
+                var htmlData = angular.element('<span></span>')
+                    .attr('ng-class', '{ \'positive\': items.length }')
+                    .html('{{items.length}}')
+                    .wrap('<div></div>')
+                    .parent();
+                td.append(htmlData);
+                tr.append(td);
+            }
+            return function($scope, $element) {
+                $compile($element.contents())($scope);
+            };
+        }
+    };
+
+
+});
